e188d7cbf47233015905a8bdb207e07d51a0ae26
teddytsen <teddytsen@gmail.com>
2014-03-29 20:43:45 +0800

    initial aokp 4.4

--- codina.mk
+++ codina.mk
@@ -5,8 +5,8 @@ $(call inherit-product, device/samsung/u8500-common/common.mk)
 $(call inherit-product, vendor/samsung/u8500-common/codina/codina-vendor-blobs.mk)
 
 ifneq ($(TARGET_SCREEN_HEIGHT),800)
-# Call cm.mk because somehow it's not being called!
-$(call inherit-product, device/samsung/codina/cm.mk)
+# Call aokp.mk because somehow it's not being called!
+$(call inherit-product, device/samsung/codina/aokp.mk)
 endif
 
 LOCAL_PATH := device/samsung/codina
